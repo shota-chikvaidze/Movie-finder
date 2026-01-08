20ccf30d7eab8234f6f36ae7af3d5a03b74ec226
shota chikvaidze <shchikvaidze1@gmail.com>
2026-01-08 18:41:28 +0400

    password not required

--- back/models/User.js
+++ back/models/User.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: function() { return this.provider === 'local' } },
     avatar: { type: String },
     provider: { type: String, enum: ['local', 'google'], default: 'local' },
     favorite: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
